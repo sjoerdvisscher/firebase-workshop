45a0ba141e7097fe1427591933f55512170f65c1
Sjoerd Visscher <sjoerd@w3future.com>
2015-07-01 19:02:43 +0200

    Listen to child_changed

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-var Root = new Firebase("https://vivid-heat-8925.firebaseio.com/");
-var Clients = Root.child("clients");
+var Root = new Firebase('https://vivid-heat-8925.firebaseio.com/');
+var Clients = Root.child('clients');
 
 Clients.on('child_added', function (snapshot) {
   var item = snapshot.val();
   var id = snapshot.key();
-  var li = $("<li>").appendTo("#clients").attr("id", "client" + id);
+  var div = $('<div>').appendTo('#clients').attr('id', 'client' + id);
   if (myId == id || !myId) {
-    $("<input>").appendTo(li).val(item.name).on("change", function() {
-      Clients.child(id).child("name").set(this.value);
+    $('<input>').appendTo(div).val(item.name).on('change', function() {
+      Clients.child(id).child('name').set(this.value);
     })
   } else {
-    li.text(item.name);
+    $('<span>').appendTo(div).text(item.name);
   }
+  updateClient(snapshot);
 })
+
+Clients.on('child_changed', updateClient);
 
 Clients.on('child_removed', function (snapshot) {
-  $("#client" + snapshot.key()).remove();
+  $('#client' + snapshot.key()).remove();
 })
 
+function updateClient(snapshot) {
+  var item = snapshot.val();
+  var id = snapshot.key();
+  var div = $('#client' + id)
+  div.css({
+    left: item.x + '00%',
+    top: item.y + '00%'
+  });
+  div.find("span").text(item.name);
+}
+
 var myId = Clients.push({
-  name: "New user",
+  name: 'New user',
   lives: 5,
-  kills: 0
+  kills: 0,
+  x: Math.floor(Math.random()*20),
+  y: Math.floor(Math.random()*10)
 }).key();
 
 window.onunload = function() {
