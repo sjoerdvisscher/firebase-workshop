b9f7353bf1793eb7660f95f30366c6271c90e6d5
Sjoerd Visscher <sjoerd@w3future.com>
2015-07-01 17:33:51 +0200

    Restart. Firebase + jquery only

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-var Vue = require('vue')
-var Firebase = require('./firebase.js')
+var Root = new Firebase("https://vivid-heat-8925.firebaseio.com/");
+var Clients = Root.child("clients");
 
-var vueFire = require('vue-fire');
-Vue.use(vueFire, { app: 'vivid-heat-8925' });
+Clients.on('child_added', function (snapshot) {
+  var item = snapshot.val();
+  var id = snapshot.key();
+  var li = $("<li>").appendTo("#clients").attr("id", "client" + id);
+  if (myId == id || !myId) {
+    $("<input>").appendTo(li).val(item.name).on("change", function() {
+      Clients.child(id).child("name").set(this.value);
+    })
+  } else {
+    li.text(item.name);
+  }
+})
 
-var appOptions = require('./app.vue')
-var app = new Vue(appOptions).$mount('#app')
+Clients.on('child_removed', function (snapshot) {
+  $("#client" + snapshot.key()).remove();
+})
+
+var myId = Clients.push({
+  name: "New user",
+  lives: 5,
+  kills: 0
+}).key();
+
+window.onunload = function() {
+  Clients.child(myId).remove();
+}
